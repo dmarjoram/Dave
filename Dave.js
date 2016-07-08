c5c9d81e9f160a92569f3d38e4c10415a07503cb
Darrell Marjoram <dmarjoram@users.noreply.github.com>
2016-07-08 09:06:43 +0100

    Dave is back.

---
+++ Dave.js
@@ -0,0 +1,30 @@
+(function () {
+    var Dave = function (params) {
+        return new dave(params);
+    };
+     
+    var dave = function (params) {
+        var selector = document.querySelectorAll(params), i = 0;
+
+        this.length = selector.length;
+		
+        this.version = '1.0.3';
+         
+        for (; i < this.length; i++) {
+            this[i] = selector[i];
+        }
+         
+        return this;        
+    };
+ 
+    Dave.fn = dave.prototype = {
+        dave: function () {
+			window.document.write('Dave.');
+            return this;
+        }
+     };
+ 
+    if (!window.Dave) {
+        window.Dave = dave;
+    }
+})();
